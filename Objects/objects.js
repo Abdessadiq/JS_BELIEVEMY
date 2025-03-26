8160ff54b3c4129ce10ef713f1f64229a183832e
Abdessadiq <abdoubhm2018@gmail.com>
2025-03-26 22:22:29 +0000

    main

--- Objects/objects.js
+++ Objects/objects.js
@@ -1,46 +1,56 @@
-let nombers = [ 10,20,45,23,10,10,10];
- let monSet = new Set(nombers);
+let nombers = [10, 20, 45, 23, 10, 10, 10];
+let monSet = new Set(nombers);
 // affiche Set(4) {size: 4, 10, 20, 45, 23}
-// console.log(monSet); 
- let monSet1 = new Set();
- monSet1.add('800');
- monSet1.add(400);
- monSet1.delete(400);
+// console.log(monSet);
+let monSet1 = new Set();
+monSet1.add("800");
+monSet1.add(400);
+monSet1.delete(400);
 
 // console.log( monSet1.size);
 //  console.log(monSet1);
 
-
-
 let users = new Map();
 // Pour ajouter une valeur
-users.set('marck Zukerburg', {
-    email : 'mark@facebook.com',
-    poste : 'PDG'
+users.set("marck Zukerburg", {
+  email: "mark@facebook.com",
+  poste: "PDG",
 });
-users.set('Bill Gates', {
-    email : 'bill@micros.com',
-    poste : 'souver le monde'
+users.set("Bill Gates", {
+  email: "bill@micros.com",
+  poste: "souver le monde",
 });
 
 // console.log(users);
 
 let voitureA = {
-    owner : 'Tesla',
-    mark : 'Cybertruck',
-}
+  owner: "Tesla",
+  mark: "Cybertruck",
+};
 let voitureB = {
-    owner : 'Renault',
-    mark : 'Espace',
-}
+  owner: "Renault",
+  mark: "Espace",
+};
 let voitures = new WeakSet([voitureA, voitureB]);
-console.log(voitures);
+// console.log(voitures);
 
 let voitureC = {
-    owner : 'Peaugeau',
-    mark : '308',
-}
+  owner: "Peaugeau",
+  mark: "308",
+};
 voitures.add(voitureC);
-console.log(voitures);
+// console.log(voitures);
 voitures.delete(voitureA);
-console.log(voitures);
+// console.log(voitures);
+
+let cars = new WeakMap();
+let index = {
+  id: 12,
+};
+let carA = {
+  owner: "Testa",
+  mark: "Cybertract",
+};
+cars.set(index, carA);
+console.log(cars);
+// cars.delete(index);
